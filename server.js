4a6a58cd1d871e1c0105a608950ad1085c971342
Adam <adamgjohnson92@gmail.com>
2023-07-03 15:55:48 -0700

    post tutor session commit. Routes are now in a separate JS folder/file from server.js. Get Post and Delete routes are all working.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs')
-const notesDB = require('./db/db.json');
+const apiRoutes = require('./routes/apiRoutes')
 
 const PORT = 3001;
 
@@ -12,66 +11,15 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
+app.use('/api', apiRoutes)
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-// let notes = [
-//     {title: 'Test',
-//     text: 'Note'},
-//     {title: "Another",
-//     text: 'Test Note'}
-// ]
-
-app.get('/api/notes', (req, res) => {
-    res.status(200).json(`${req.method} request received to render notes.`)
-
-    console.info(`${req.method} request received to render notes.`)
-});
-
-//Where does the /api come from? (I based this off of examples from class)
-
-app.post('/api/notes', (req, res) => {
-    console.log(`${req.method} request recieved to add a note.`)
-    const { title, text } = req.body;
-    
-    if (id && title && text) {
-    let newNote = {
-        id:req.body.id + 1,
-        title: req.body.title,
-        text: req.body.text
-    }
-
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if(err) {
-            console.error(err)
-        } else {
-            const parsedNotes = JSON.parse(data);
-            parsedNotes.push(newNote);
-
-        fs.writeFile('./db/db.json', 
-        
-        JSON.stringify(parsedNotes, null, 4),
-        (writeErr) => writeErr
-        ? console.error(writeErr)
-        : console.info('Successfully added a note!')
-        );
-    }
-    });
-
-    const response = {
-        status: 'success',
-        body: newNote
-    };
-
-    console.log(response);
-    res.status(201).json(response);
-    } else {
-     res.status(500).json('Error in creating new note.')
-    }
-});
-
-
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
 
 
 app.listen(PORT, () => 
